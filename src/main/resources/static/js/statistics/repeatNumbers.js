70a0a07861edc4e3424865869215d378e0e14971
Rostislav <rostix2000@gmail.com>
2020-02-06 22:41:07 +0200

    Added plots to statistic page

--- src/main/resources/static/js/statistics/repeatNumbers.js
+++ src/main/resources/static/js/statistics/repeatNumbers.js
@@ -1,14 +1,68 @@
-fetch("http://localhost:8082/createStatistic/statisticByUserForRepeatNumbers")
-    .then(response => {
-        return response.json()
-    })
-    .then(result => {
-        console.log(result);
-        for (let i of result){
-            console.log(i)
-        }
-    })
-    .catch(err => {
-        console.log(err)
-    });
+async function getStats(testType) {
+    let url = '/createStatistic/statisticByUserForRepeatNumbers?type=' + testType;
+    return await fetch(url)
+        .then(response => {
+            return response.json()
+        })
+        .then(result => {
+            return result
+        })
+        .catch(err => {
+            console.log(err)
+        })
+}
 
+function processData(data) {
+    /**
+     * Count how many results in ranges 0-9, 10-19, 20-29, ..., 90-100.
+     * @return {Array} array of 10 elements which says upper mentioned information
+     */
+    // todo rewrite this code in dynamic style, because now it's govno sobaki
+    let result = [];
+    let lowerBound = 0;
+    let upperBound = 9;
+    for (let i = 0; i < 10; i++) {
+        result.push(data.filter(score =>{
+            return (score >= lowerBound) && (score <= upperBound)
+        }));
+        lowerBound += 10;
+        upperBound += 10;
+    }
+    result = result.map(function (arr) {
+        return arr.length
+    });
+    return result
+
+}
+
+function plot(yData, containerId, testTitle) {
+    let xData = ['0-9', '10-19', '20-29', '30-39', '40-49', '50-59', '60-69', '70-79', '80-89', '90-100'];
+    const container1 = document.getElementById(containerId);
+    const trace1 = {
+        x: xData,
+        y: yData,
+        type: 'bar',
+        marker: {
+            color: 'rgb(75, 111, 255)',
+            opacity: 0.7,
+        }
+    };
+    const data = [trace1];
+    const layout = {
+        title: testTitle,
+        barmode: 'group'
+    };
+    Plotly.newPlot(container1, data, layout);
+}
+
+async function main(){
+    const response1 = await getStats('RepeatNumbersTest');
+    const data1 = processData(response1);
+    plot(data1, 'plot-1', 'Repeat Numbers');
+
+    const response2 = await getStats('CountGreenTest');
+    const data2 = processData(response2);
+    plot(data2, 'plot-2', 'Count Green');
+}
+
+window.onload = main;
