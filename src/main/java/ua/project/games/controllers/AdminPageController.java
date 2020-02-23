c38a0bfb97f08eb3a668f3411e54d58136341665
Ivaschenko Pavlo <pawloiwanov@gmai.com>
2020-02-23 21:57:43 +0200

    [add]: admin page with users

--- src/main/java/ua/project/games/controllers/AdminPageController.java
+++ src/main/java/ua/project/games/controllers/AdminPageController.java
@@ -1,35 +1,20 @@
 package ua.project.games.controllers;
 
-import org.apache.catalina.startup.ClassLoaderFactory;
-import org.hibernate.cfg.beanvalidation.GroupsPerOperation;
-import org.reflections.Reflections;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cglib.proxy.InterfaceMaker;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 import org.springframework.data.repository.support.Repositories;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.WebApplicationContext;
-import ua.project.games.anotations.AdminRepo;
 import ua.project.games.entity.User;
-import ua.project.games.repository.TestStatisticRepository;
 import ua.project.games.repository.UserRepository;
 
 import javax.persistence.EntityManager;
-import javax.persistence.metamodel.Attribute;
-import javax.persistence.metamodel.EntityType;
 import javax.sql.DataSource;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
 import java.security.Principal;
-import java.util.Arrays;
 import java.util.Set;
 
 @Controller
@@ -42,15 +27,17 @@ public class AdminPageController {
     private final WebApplicationContext appContext;
     private final Set<?> classes;
     Repositories repositories = null;
-    private User user = new User();
 
+    @Autowired
+    private final UserRepository userRepository;
 
-    public AdminPageController(DataSource dataSource, EntityManager entityManager, WebApplicationContext appContext) {
+    public AdminPageController(DataSource dataSource, EntityManager entityManager, WebApplicationContext appContext, UserRepository userRepository) {
         this.dataSource = dataSource;
         this.entityManager = entityManager;
         this.appContext = appContext;
         this.classes = entityManager.getMetamodel().getEntities();
         this.repositories = new Repositories(appContext);
+        this.userRepository = userRepository;
     }
 
 
@@ -61,24 +48,41 @@ public class AdminPageController {
         return "/admin/adminIndex";
     }
 
-    @GetMapping(value = "/{entity}")
-    public String getRepeatNumbersTest(Principal principal, Model model, @PathVariable String entity){
-        Reflections ref = new Reflections("");
-        for (
-                Class<?> cl : ref.getTypesAnnotatedWith(AdminRepo.class)) {
-            AdminRepo findable = cl.getAnnotation(AdminRepo.class);
-            if (findable.name().getSimpleName().equals(entity)) {
-                Class<?> clazz = findable.name();
-                System.out.println(clazz.toString());
-                entityManager.getMetamodel().entity(clazz).getAttributes().stream().map(Attribute::getName).forEach(System.out::println);
-                System.out.println(cl.getSimpleName());
-            }
-        }
+    @GetMapping(value = "/User")
+    public String getUsers (Model model, Principal principal) {
+        model.addAttribute("username", principal.getName());
+        model.addAttribute("users", userRepository.findAll());
+        return "admin/adminUser";
 
-        final String currentUserName = SecurityContextHolder.getContext().getAuthentication().getName();
-        model.addAttribute("username", currentUserName);
-        return "/admin/adminIndex";
     }
+
+    @PostMapping(value = "/User/delete/{user_id}")
+    public String deleteUser(@PathVariable String user_id) {
+        User userToDelete = userRepository.findById(Long.parseLong(user_id)).get();
+        userRepository.delete(userToDelete);
+        return "redirect:/admin/User";
+    }
+
+    //TODO important!
+
+//    @GetMapping(value = "/{entity}")
+//    public String getRepeatNumbersTest(Principal principal, Model model, @PathVariable String entity){
+//        Reflections ref = new Reflections("");
+//        for (
+//                Class<?> cl : ref.getTypesAnnotatedWith(AdminRepo.class)) {
+//            AdminRepo findable = cl.getAnnotation(AdminRepo.class);
+//            if (findable.name().getSimpleName().equals(entity)) {
+//                Class<?> clazz = findable.name();
+//                System.out.println(clazz.toString());
+//                entityManager.getMetamodel().entity(clazz).getAttributes().stream().map(Attribute::getName).forEach(System.out::println);
+//                System.out.println(cl.getSimpleName());
+//            }
+//        }
+//
+//        final String currentUserName = SecurityContextHolder.getContext().getAuthentication().getName();
+//        model.addAttribute("username", currentUserName);
+//        return "/admin/adminIndex";
+//    }
 }
 
 
